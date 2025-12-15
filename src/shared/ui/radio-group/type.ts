f3541e08d24dce21c4db490778d0f3afab0accd5
Spartan <mrkillerrate775@gmail.com>
2025-12-15 23:26:19 +0300

    feat: реализовал RadioGroupUI

---
+++ src/shared/ui/radio-group/type.ts
@@ -0,0 +1,12 @@
+export interface RadioOption {
+  value: string;
+  label: string;
+}
+
+export interface RadioGroupProps {
+  title?: string;
+  options: RadioOption[];
+  value: string;
+  onChange: (value: string) => void;
+  name: string;
+}
