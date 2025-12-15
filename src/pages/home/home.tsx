909b057783a4bf4cc72bf560f78f9e431bdf619e
Vovchensky <vovchensky.pt@bk.ru>
2025-12-15 22:00:40 +0700

    feat: добавлены заглушки для страниц

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,4 @@
+import type { FC } from 'react';
+import styles from './home.module.css';
+
+export const HomePage: FC = () => null;
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { HomePage } from './home';
+export { SkillPage } from './skill';
+export { ProfilePage } from './profile';
+export { NotFoundPage } from './not-found';
+export { ServerErrorPage } from './server-error';
+export { LoginPage } from './login';
+export { RegisterPage } from './register';
