ad13448d8c4da4d7c4dbe3f9998a92f75f5f3f57
Alexey Semikopeykin <a7sept@yandex.ru>
2025-12-22 12:37:59 +0400

    feat(components): add-footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,32 @@
+import { LogoUI } from '@ui/logo';
+import { NavElementUI } from '@ui/nav-element-ui';
+import styles from './footer.module.css';
+
+export const FooterLayout = () => (
+  <footer className={styles.footer}>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <LogoUI />
+      </div>
+
+      <nav className={styles.navBlock} aria-label="Навигация по сайту">
+        <div className={styles.navColumn}>
+          <NavElementUI text="О проекте" onClick={() => {}} />
+          <NavElementUI text="Все навыки" onClick={() => {}} />
+        </div>
+
+        <div className={styles.navColumn}>
+          <NavElementUI text="Контакты" onClick={() => {}} />
+          <NavElementUI text="Блог" onClick={() => {}} />
+        </div>
+
+        <div className={styles.navColumn}>
+          <NavElementUI text="Политика конфиденциальности" onClick={() => {}} />
+          <NavElementUI text="Пользовательское соглашение" onClick={() => {}} />
+        </div>
+      </nav>
+
+      <p className={styles.copyright}>SkillSwap — 2025</p>
+    </div>
+  </footer>
+);
--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { HeaderUI } from '@components/header';
+import { FooterLayout } from '@components/footer';
 
 export const MainLayout = () => {
   /* я добавила состояние, чтобы корректно работал поиск, но потом нужно будет перенести это в умный компонент для шапки */
@@ -16,25 +17,13 @@ export const MainLayout = () => {
         onSearchClear={() => setSearchValue('')}
       />
 
-      {/*
-      <HeaderUI
-        variant="full"
-        isUserAuth
-        userName="Мария"
-        searchValue={searchValue}
-        onSearchChange={setSearchValue}
-        onSearchClear={() => setSearchValue('')}
-      />
-      */}
-
       <main>
         <Suspense fallback="Загружаем...">
           <Outlet />
         </Suspense>
       </main>
 
-      {/* TODO: когда появится футер, импортировать его и использовать здесь <AppFooter /> */}
-      <h1>Футер</h1>
+      <FooterLayout />
     </>
   );
 };
