67fa4c0f0814a78cf2dd8badf5df13ae039d3510
i1off-tape <jhoncrendy18@gmail.com>
2025-12-24 05:59:11 +0500

    fix(prettier): был запущен script format

--- src/shared/ui/button/types.ts
+++ src/shared/ui/button/types.ts
@@ -2,8 +2,7 @@ import { ReactNode, ButtonHTMLAttributes, CSSProperties } from 'react';
 
 export type ButtonUIVariant = 'primary' | 'secondary' | 'tertiary';
 
-export interface ButtonUIProps
-  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
+export interface ButtonUIProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
   children: ReactNode;
   variant?: ButtonUIVariant;
   padding?: CSSProperties['padding'];
