ffcf213a5b73287ac9e415c8286da77a00d0254b
Alexey Semikopeykin <a7sept@yandex.ru>
2026-01-05 12:41:45 +0300

    feat(pages): add_404_and_500_pages

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,6 +1,46 @@
 import type { FC } from 'react';
-// import styles from './not-found.module.css';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const NotFoundPage: FC = () => <h1>Not found Page</h1>;
+import { IllustrationBlockUI } from '@ui/illustration-block';
+import Error404Illustration from '@images/error-404';
+
+import styles from './not-found.module.css';
+
+export const NotFoundPage: FC = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
+
+  const handleReportError = useCallback(() => {
+    // тут заглушка
+    console.log('Сообщить об ошибке');
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <IllustrationBlockUI
+          illustration={<Error404Illustration />}
+          title="Страница не найдена"
+          description="К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже"
+          actions={[
+            {
+              text: 'Сообщить об ошибке',
+              onClick: handleReportError,
+            },
+            {
+              primary: true,
+              text: 'На главную',
+              onClick: handleGoHome,
+            },
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default NotFoundPage;
